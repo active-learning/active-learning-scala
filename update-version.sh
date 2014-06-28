2cbead86c9857630774bb4a3e57874ff27f7f026
davips <davips@users.noreply.github.com>
2014-06-27 22:36:31 -0300

    counting version on davips commits

--- update-version.sh
+++ update-version.sh
@@ -1,3 +1,3 @@
-echo `expr $(git shortlog -s -n | cut -d'd' -f1 | head -n1) + 1` > minV
+echo `expr $(git shortlog -s -n | grep davips | cut -d'd' -f1 | head -n1) + 1` > minV
 echo $(cat majV).$(cat minV) > VERSION
 cat VERSION
