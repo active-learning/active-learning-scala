214e5f3f3b4a31ec57f90fb20b20fb047863e5e7
davips <davips@users.noreply.github.com>
2014-07-01 14:05:44 -0300

    learner() devidamente com seeds

--- src/main/scala/app/ArgParser.scala
+++ src/main/scala/app/ArgParser.scala
@@ -72,11 +72,18 @@ object ArgParser {
     }
     def learner(Lmax: Int, seed: Int, pool: Seq[Pattern]) = args(3) match {
       case "NB" => NB()
-      case "CI" => CIELM(Lmax * 2)
-      case "ECI" => ECIELM(Lmax * 2)
-      case "I" => IELM(Lmax * 2)
-      case "EI" => EIELM(Lmax * 2)
-      case "intera" => interaELM(Lmax / 3)
+
+      case "CI" => CIELM(Lmax * 2, seed)
+      case "ECI" => ECIELM(Lmax * 2, seed)
+      case "I" => IELM(Lmax * 2, seed)
+      case "EI" => EIELM(Lmax * 2, seed)
+
+      case "CI2" => CIELM(Lmax * 2, seed, "2")
+      case "ECI2" => ECIELM(Lmax * 2, seed, "2")
+      case "I2" => IELM(Lmax * 2, seed, "2")
+      case "EI2" => EIELM(Lmax * 2, seed, "2")
+
+      case "intera" => interaELM(Lmax / 3, seed)
       case "C45" => C45()
       case "VFDT" => VFDT()
       case "1NNc" => KNN(1, "cheb", pool)
