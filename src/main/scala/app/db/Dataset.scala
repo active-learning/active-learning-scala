58ca2509c6d3c36372701152bde37b411b8e31f4
davips <davips@users.noreply.github.com>
2014-07-25 02:21:50 -0300

    sgmulti tested resume

--- src/main/scala/app/db/Dataset.scala
+++ src/main/scala/app/db/Dataset.scala
@@ -194,15 +194,12 @@ case class Dataset(path: String, createOnAbsence: Boolean = false, readOnly: Boo
     }
 
     //Get queries from past jobs, if any.
-    println("getQs")
     val queries = fetchQueries(strat, run, fold, f)
     val nextPosition = queries.size
-    println(s"nexP:$nextPosition")
     if (nextPosition < Q && nextPosition < strat.pool.size) {
-      val (nextIds, t) = Tempo.timev(strat.timeLimitedResumeQueries(queries, seconds).take(Q - nextPosition).map(_.id).toVector)
+      val (nextIds, t) = if (nextPosition == 0) Tempo.timev(strat.timeLimitedQueries(seconds).take(Q).map(_.id).toVector)
+      else Tempo.timev(strat.timeLimitedResumeQueries(queries, seconds).take(Q - nextPosition).map(_.id).toVector)
       q = nextIds.length
-      //    println(s"q:$q")
-      //    println(s"Q:$Q")
       acquire()
       var str = ""
       try {
