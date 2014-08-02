99c7ce7b82662c7437c34e98bda1b12c23bbaf35
davips <davips@users.noreply.github.com>
2014-08-02 04:02:58 -0300

    rnd hits

---
+++ src/main/scala/exp/raw/RandomHits.scala
@@ -0,0 +1,43 @@
+/*
+ active-learning-scala: Active Learning library for Scala
+ Copyright (c) 2014 Davi Pereira dos Santos
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package exp.raw
+
+import al.strategies._
+import app.ArgParser
+import app.db.Dataset
+import ml.Pattern
+import util.Datasets
+import weka.filters.unsupervised.attribute.Standardize
+
+object RandomHits extends CrossValidation with App {
+  val samplingSize = 500
+  val desc = "Version " + ArgParser.version + " \n Generates confusion matrices for queries (from hardcoded rnd strategy) for the given list of datasets."
+  val (path, datasetNames, learner) = ArgParser.testArgsWithLearner(className, args, desc)
+  val parallelDatasets = args(2).contains("d")
+  val parallelRuns = args(2).contains("r")
+  val parallelFolds = args(2).contains("f")
+  val parallelStrats = args(2).contains("s")
+  val source = Datasets.patternsFromSQLite(path) _
+  val dest = Dataset(path) _
+
+  run { (db: Dataset, run: Int, fold: Int, pool: Seq[Pattern], testSet: Seq[Pattern], f: Standardize) =>
+    val nc = pool.head.nclasses
+    db.saveHits(RandomSampling(Seq()), learner(pool.length / 2, run, pool), run, fold, nc, f, testSet)
+  }
+}
