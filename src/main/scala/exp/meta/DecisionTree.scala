095b49c68766a6e2205be5cc4cb7ccebbe21830f
davips <davips@users.noreply.github.com>
2014-08-31 01:20:25 -0300

    pattern hashcode is now a val

---
+++ src/main/scala/exp/meta/DecisionTree.scala
@@ -0,0 +1,49 @@
+/*
+ active-learning-scala: Active Learning library for Scala
+ Copyright (c) 2014 Davi Pereira dos Santos
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package exp.meta
+
+import app.ArgParser
+import app.db.entities.Dataset
+
+/**
+ * Created by davi on 09/06/14.
+ */
+object DecisionTree extends App {
+  val desc = s"Version ${ArgParser.version} \n Identifica vencedores em cada base para servirem de rótulo num metadataset com metaatributos que será gerado."
+  val (path, datasetNames) = ArgParser.testArgs(getClass.getSimpleName.dropRight(1), args, 3, desc)
+  val parallel = args(2) == "y"
+  val readOnly = false
+  val runs = Dataset("")("").runs
+  val folds = Dataset("")("").folds
+  val dest = Dataset(path, createOnAbsence = false, readOnly) _
+  val qname = (if (parallel) datasetNames.par else datasetNames).toList map { datasetName =>
+    val db = dest(datasetName)
+    if (db.dbOriginal.exists()) {
+      db.open()
+      val Q = db.Q
+      println(s"$Q $datasetName ${db.n}")
+      db.close()
+      (Q, datasetName)
+    } else (-1, datasetName)
+  }
+  qname.sortBy(_._1) foreach println
+  println("")
+  println(qname.sortBy(_._1).map(_._2).mkString(","))
+  println("")
+}
