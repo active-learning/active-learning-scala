c9d59d4f685f7c243f279aeb3f3387a934797af4
davips <davips@users.noreply.github.com>
2014-09-01 02:04:51 -0300

    releaseOp() nonredundant

--- src/main/scala/app/db/entities/Dataset.scala
+++ src/main/scala/app/db/entities/Dataset.scala
@@ -335,13 +335,13 @@ case class Dataset(path: String, createOnAbsence: Boolean = false, readOnly: Boo
         val (nextIds, t) = if (nextPosition == 0) Tempo.timev(strat.timeLimitedQueries(seconds, exiting).take(Q).map(_.id).toVector)
         else Tempo.timev(strat.timeLimitedResumeQueries(queries, seconds, exiting).take(Q - nextPosition).map(_.id).toVector)
         q = nextIds.length
+        acquireOp()
         if (nextPosition == 0 && q < nclasses) {
           println(s"Interrupted querying did not get enough queries to write ($q < $nclasses).")
           0
         } else {
           if (allWin && nextPosition + q != Q) println(s"In allWin mode: ($nextPosition + $q) generated queries != $Q expected queries; ignoring write request.")
           else {
-            acquireOp()
             println(s"Gravando queries para $dataset pool: $run.$fold ...")
             var str = ""
             try {
@@ -366,10 +366,10 @@ case class Dataset(path: String, createOnAbsence: Boolean = false, readOnly: Boo
             }
             println(s"${Calendar.getInstance().getTime} $q $strat queries written to " + dbCopy + s" at $run.$fold. Backing up tmpFile...")
             save()
-            releaseOp()
           }
           nextPosition + q
         }
+        releaseOp()
       } else nextPosition
       Some(r)
     }
