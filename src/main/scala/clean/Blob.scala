65efb06b77d88a67f897cf339b6b7843797a129b
davips <davips@users.noreply.github.com>
2014-09-13 21:48:50 -0300

    create h table com blob

---
+++ src/main/scala/clean/Blob.scala
@@ -0,0 +1,27 @@
+package clean
+
+import java.util.Calendar
+
+/*
+active-learning-scala: Active Learning library for Scala
+Copyright (c) 2014 Davi Pereira dos Santos
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+trait Blob {
+  def zip(numbers: Seq[Int]) = numbers flatMap (n => Integer.toBinaryString(n).reverse.padTo(12, 0).reverse)
+
+  def unzip(bytes: Array[Byte]) = (bytes grouped 12).map(_.mkString).map(Integer.parseInt(_, 2)).toList
+}
