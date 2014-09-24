eccb928119800c8acedd99d29d9fbd3bf00753a0
davips <davips@users.noreply.github.com>
2014-09-24 12:04:23 -0300

    sem par

--- src/test/scala/clean/TopSpec.scala
+++ src/test/scala/clean/TopSpec.scala
@@ -58,7 +58,7 @@ class TopSpec extends UnitSpec with Blob with Lock {
 
   val run = 0
   val asserts = mutable.Queue[Boolean]()
-  datasets.filter(!_.startsWith("#")).par foreach { dataset =>
+  datasets.filter(!_.startsWith("#")) foreach { dataset =>
     val ds = Ds(path, dataset)
     ds.open()
     ds.log(s"Processing ${ds.n} instances ...")
