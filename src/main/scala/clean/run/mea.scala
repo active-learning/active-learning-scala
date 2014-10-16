b20eaa6b2bf4da0274d652e38b1c5140d481786d
davips <davips@users.noreply.github.com>
2014-10-16 13:45:52 -0300

    mea agora faz allMeasures

--- src/main/scala/clean/run/mea.scala
+++ src/main/scala/clean/run/mea.scala
@@ -21,61 +21,63 @@ package clean.run
 
 import al.strategies.{RandomSampling, Strategy}
 import clean._
+import clean.res._
 import ml.Pattern
 import ml.classifiers.Learner
 import weka.filters.Filter
 
 import scala.collection.mutable
 
-object mea extends Exp with LearnerTrait with StratsTrait with Lock with CM {
+object mea extends Exp with LearnerTrait with StratsTrait with Lock with CM with MeasuresTrait {
   val context = "meaApp"
-  val arguments = superArguments ++ List("medida:alca|alcg")
+  val arguments = superArguments
   val ignoreNotDone = false
   val sqls = mutable.Queue[String]()
   run()
 
-  def calculate(ds: Ds, cms: mutable.LinkedHashMap[Int, Array[Array[Int]]], tsSize: Int) = measure.calc(ds, cms, tsSize)
+  def calculate(ds: Ds, cms: mutable.LinkedHashMap[Int, Array[Array[Int]]], tsSize: Int, meas: Measure) = meas.calc(ds, cms, tsSize)
 
   def op(ds: Ds, pool: Seq[Pattern], testSet: Seq[Pattern], fpool: Seq[Pattern], ftestSet: Seq[Pattern], learnerSeed: Int, run: Int, fold: Int, binaf: Filter, zscof: Filter) {
     if (!ds.isQCalculated) ds.error(s"Q is not calculated!")
-    else if (!sqls.contains("cancel")) {
-      if (measure.id == 15 || measure.id == 16) {
-        specialLearners(Seq()) foreach storeSQL(pool.size, ds, RandomSampling(Seq()), run, fold, testSet.size)
+    else allMeasures foreach { meas => if (!sqls.contains("cancel")) {
+      if (meas.id == 15 || meas.id == 16) {
+        specialLearners(Seq()) foreach storeSQL(pool.size, ds, RandomSampling(Seq()), run, fold, testSet.size, meas)
       } else {
         stratsemLearnerExterno() foreach { strat =>
           if (!ds.areQueriesFinished(pool.size, strat, run, fold, null, null, completeIt = false)) {
             ds.log(s"Queries were not finished for ${strat.abr}/${strat.learner} at pool $run.$fold!")
             sqls += "cancel"
-          } else if (strat.id >= 17 && strat.id <= 21) storeSQL(pool.size, ds, strat, run, fold, testSet.size)(strat.learner)
-          else allLearners() foreach storeSQL(pool.size, ds, strat, run, fold, testSet.size)
+          } else if (strat.id >= 17 && strat.id <= 21) storeSQL(pool.size, ds, strat, run, fold, testSet.size, meas)(strat.learner)
+          else allLearners() foreach storeSQL(pool.size, ds, strat, run, fold, testSet.size, meas)
         }
         allLearners() foreach { learner =>
           stratcomLearnerExterno(learner) foreach { strat =>
             if (!ds.areQueriesFinished(pool.size, strat, run, fold, null, null, completeIt = false)) {
               ds.log(s"Queries were not finished for ${strat.abr}/${strat.learner} at pool $run.$fold!")
               sqls += "cancel"
-            } else storeSQL(pool.size, ds, strat, run, fold, testSet.size)(learner)
+            } else storeSQL(pool.size, ds, strat, run, fold, testSet.size, meas)(learner)
           }
         }
       }
     }
+    }
   }
 
-  def storeSQL(poolSize: Int, ds: Ds, strat: Strategy, run: Int, fold: Int, testSetSize: Int)(learner: Learner): Unit = {
+  def storeSQL(poolSize: Int, ds: Ds, strat: Strategy, run: Int, fold: Int, testSetSize: Int, meas: Measure)(learner: Learner): Unit = {
     log(s"$strat $learner $run $fold")
     if (!ds.areHitsFinished(poolSize, Seq(), strat, learner, run, fold, null, null, completeIt = false)) {
       ds.log(s"Conf. matrices were not finished for ${strat.abr}/$learner/svm? at pool $run.$fold!")
       sqls += "cancel"
-    } else ds.getMeasure(measure, strat, learner, run, fold) match {
-      case Some(_) => log(s"Measure $measure already calculated for ${strat.abr}/${strat.learner} at pool $run.$fold!")
+    } else ds.getMeasure(meas, strat, learner, run, fold) match {
+      case Some(_) => log(s"Measure $meas already calculated for ${strat.abr}/${strat.learner} at pool $run.$fold!")
       case None =>
         val cms = ds.getCMs(strat, learner, run, fold)
         if (cms.size < ds.Q - ds.nclasses + 1) ds.quit(s"Couldn't take at least ${ds.Q - ds.nclasses + 1} queries, ${cms.size} only.")
         val tsSize = contaTotal(cms.head._2)
         if (testSetSize != tsSize) ds.quit("Hits differs from testSetSize!")
-        val v = calculate(ds, cms, tsSize)
+        val v = calculate(ds, cms, tsSize, meas)
         acquire()
-        sqls += ds.measureToSQL(measure, v, strat.id, learner, run, fold)
+        sqls += ds.measureToSQL(meas, v, strat.id, learner, run, fold)
         release()
     }
   }
