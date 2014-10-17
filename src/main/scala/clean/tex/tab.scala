00a8809b2f815a914937272c8fd0e9f18eb68ea1
davips <davips@users.noreply.github.com>
2014-10-17 16:47:03 -0300

    passgme order tab Q

--- src/main/scala/clean/tex/tab.scala
+++ src/main/scala/clean/tex/tab.scala
@@ -107,9 +107,9 @@ object tab extends AppWithUsage with LearnerTrait with StratsTrait with Measures
       println(s"")
       println(s"")
 
-      //      val tbs = res.map(x => x._1 -> x._2.padTo(sl.size, (-1d, -1d))).sortBy(_._1) grouped 50
-      val tbs = res.map(x => x._1 -> x._2.padTo(sl.size, (-1d, -1d))).toList grouped 50
-      //      val tbs = res.map(x => x._1 -> x._2.padTo(sl.size, (-1d, -1d))).sortBy(_._2.head._1) grouped 50
+      //      val tbs = res.map(x => x._1 -> x._2.padTo(sl.size, (-1d, -1d))).toList.sortBy(_._1) grouped 50
+      //      val tbs = res.map(x => x._1 -> x._2.padTo(sl.size, (-1d, -1d))).toList grouped 50
+      val tbs = res.map(x => x._1 -> x._2.padTo(sl.size, (-1d, -1d))).toList.sortBy(_._2.head._1) grouped 50
       tbs foreach { case res0 =>
         StatTests.extensiveTable2(res0.toSeq.map(x => x._1.take(3) + x._1.takeRight(12) -> x._2), sl.toVector.map(_.toString), "nomeTab", measure.toString)
       }
