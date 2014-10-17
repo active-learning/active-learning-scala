6d7469b694c2a1b5da711f0f532e3d03137593e0
davips <davips@users.noreply.github.com>
2014-10-17 14:59:59 -0300

    completeIt

--- src/main/scala/clean/run/mea.scala
+++ src/main/scala/clean/run/mea.scala
@@ -30,7 +30,7 @@ import scala.collection.mutable
 
 object mea extends Exp with LearnerTrait with StratsTrait with Lock with CM with MeasuresTrait {
   val context = "meaApp"
-  val arguments = superArguments
+  val arguments = superArguments ++ Seq("completeQsHs?:y|n")
   val ignoreNotDone = false
   val sqls = mutable.Queue[String]()
   run()
@@ -44,7 +44,7 @@ object mea extends Exp with LearnerTrait with StratsTrait with Lock with CM with
         specialLearners(Seq()) foreach storeSQL(pool.size, ds, RandomSampling(Seq()), run, fold, testSet.size, meas)
       } else {
         stratsemLearnerExterno() foreach { strat =>
-          if (!ds.areQueriesFinished(pool.size, strat, run, fold, null, null, completeIt = false)) {
+          if (!ds.areQueriesFinished(pool.size, strat, run, fold, null, null, completeIt)) {
             ds.log(s"Queries were not finished for ${strat.abr}/${strat.learner} at pool $run.$fold!")
             //            sqls += "cancel"
           } else if (strat.id >= 17 && strat.id <= 21) storeSQL(pool.size, ds, strat, run, fold, testSet.size, meas)(strat.learner)
@@ -52,7 +52,7 @@ object mea extends Exp with LearnerTrait with StratsTrait with Lock with CM with
         }
         allLearners() foreach { learner =>
           stratcomLearnerExterno(learner) foreach { strat =>
-            if (!ds.areQueriesFinished(pool.size, strat, run, fold, null, null, completeIt = false)) {
+            if (!ds.areQueriesFinished(pool.size, strat, run, fold, null, null, completeIt)) {
               ds.log(s"Queries were not finished for ${strat.abr}/${strat.learner} at pool $run.$fold!")
               //              sqls += "cancel"
             } else storeSQL(pool.size, ds, strat, run, fold, testSet.size, meas)(learner)
@@ -65,7 +65,7 @@ object mea extends Exp with LearnerTrait with StratsTrait with Lock with CM with
 
   def storeSQL(poolSize: Int, ds: Ds, strat: Strategy, run: Int, fold: Int, testSetSize: Int, meas: Measure)(learner: Learner): Unit = {
     log(s"$strat $learner $run $fold")
-    if (!ds.areHitsFinished(poolSize, Seq(), strat, learner, run, fold, null, null, completeIt = false)) {
+    if (!ds.areHitsFinished(poolSize, Seq(), strat, learner, run, fold, null, null, completeIt)) {
       ds.log(s"Conf. matrices were not finished for ${strat.abr}/$learner/svm? at pool $run.$fold!")
       //      sqls += "cancel"
     } else ds.getMeasure(meas, strat, learner, run, fold) match {
