67e5baa8342eafb8e99527d98d65418400f34e0d
davips <davips@users.noreply.github.com>
2014-12-08 00:31:01 -0200

    log c/ hostname

--- src/main/scala/clean/Log.scala
+++ src/main/scala/clean/Log.scala
@@ -25,7 +25,7 @@ trait Log {
 
   def log(msg: String, level: Int = 10) {
     level match {
-      case x if x >= 30 - Global.debug => println(s"$context : $msg    ${Calendar.getInstance().getTime}")
+       case x if x >= 30 - Global.debug => println(s"${Runtime.getRuntime.exec("hostname")} $context : $msg    ${Calendar.getInstance().getTime}")
       case x =>
     }
   }
@@ -34,11 +34,11 @@ trait Log {
     Global.running = false
     Thread.sleep(10)
     new Throwable().printStackTrace()
-    justQuit(s"$context : Error: $msg")
+     justQuit(s"${Runtime.getRuntime.exec("hostname")} $context : Error: $msg")
   }
 
   def justQuit(msg: String) = {
-    log(s"$context : Quiting: $msg", 30)
+     log(s"${Runtime.getRuntime.exec("hostname")} $context : Quiting: $msg", 30)
     Global.running = false
     Thread.sleep(10)
     sys.exit(1)
