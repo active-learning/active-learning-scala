7bc77e158dc5ad3ad1881e8e654aab92d626fc96
davips <davips@users.noreply.github.com>
2014-12-09 04:21:01 -0200

    syntax bug insert

--- src/main/scala/clean/res/Measure.scala
+++ src/main/scala/clean/res/Measure.scala
@@ -44,7 +44,7 @@ trait Measure extends CM with Blob {
 
    def write(ds: Ds, cm: Array[Array[Int]] = null) {
       if (ds.read(s"select count(0) from r where p=$pid") == List(Vector(0))) {
-         if (cm != null) ds.write(s"insert into r values ($id, $pid, ${instantFun(cm)}")
+         if (cm != null) ds.write(s"insert into r values ($id, $pid, ${instantFun(cm)})")
          else value match {
             case Some(v) => ds.write(s"insert into r values ($id, $pid, $v)")
             case None => ds.log(s"Pool $r.$f incompleto. Impossivel calcular a medida $this.")
