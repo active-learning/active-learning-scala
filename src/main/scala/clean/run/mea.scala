8306f385e3fdabf70be398ff61a25f5033e1b6f9
davips <davips@users.noreply.github.com>
2014-12-09 00:36:42 -0200

    nao tm problema demorar 2min pra gerar fried/tabelas etc. se no futuro for necessario, jah tem os ids corretos pra tabela r (que precisa ser recriada para o que era antes).

--- src/main/scala/clean/run/mea.scala
+++ src/main/scala/clean/run/mea.scala
@@ -1,41 +1,41 @@
-///*
-//
-//active-learning-scala: Active Learning library for Scala
-//Copyright (c) 2014 Davi Pereira dos Santos
-//
-//   This program is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//*/
-//
-//package clean.run
-//
-//import clean._
-//import clean.res._
-//import ml.Pattern
-//import weka.filters.Filter
-//
-//import scala.collection.mutable
-//
-//object mea extends Exp with LearnerTrait with StratsTrait with Lock with CM with MeasuresTrait {
-//   val context = "meaApp"
-//   val arguments = superArguments
-//   val ignoreNotDone = false
-//   run()
-//
-//   def calculate(ds: Ds, cms: mutable.LinkedHashMap[Int, Array[Array[Int]]], tsSize: Int, meas: Measure) = meas.calc(ds, cms, tsSize)
-//
-//   def op(ds: Ds, pool: Seq[Pattern], testSet: Seq[Pattern], fpool: Seq[Pattern], ftestSet: Seq[Pattern], learnerSeed: Int, run: Int, fold: Int, binaf: Filter, zscof: Filter) {
-//      allMeasures foreach { measu =>
+/*
+
+active-learning-scala: Active Learning library for Scala
+Copyright (c) 2014 Davi Pereira dos Santos
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package clean.run
+
+import clean._
+import clean.res._
+import ml.Pattern
+import weka.filters.Filter
+
+import scala.collection.mutable
+
+object mea extends Exp with LearnerTrait with StratsTrait with Lock with CM with MeasuresTrait {
+   val context = "meaApp"
+   val arguments = superArguments
+   val ignoreNotDone = false
+   run()
+
+   def calculate(ds: Ds, cms: mutable.LinkedHashMap[Int, Array[Array[Int]]], tsSize: Int, meas: Measure) = meas.calc(ds, cms, tsSize)
+
+   def op(ds: Ds, pool: Seq[Pattern], testSet: Seq[Pattern], fpool: Seq[Pattern], ftestSet: Seq[Pattern], learnerSeed: Int, run: Int, fold: Int, binaf: Filter, zscof: Filter) {
+      //      allMeasures foreach { measu =>
 //         //se for passiva, só poderia medir em special learners; mas posso rodar o classificador aqui com o pool inteiro e descobrir o valor
 //         //               specialLearners(Seq()) foreach storeSQL(pool.size, ds, RandomSampling(Seq()), run, fold, testSet.size, m)
 //         stratsemLearnerExterno() foreach { strat =>
@@ -58,14 +58,14 @@
 //            }
 //         }
 //      }
-//   }
-//
-//   def datasetFinished(ds: Ds) {
-//   }
-//
-//   def isAlreadyDone(ds: Ds) = false //ds.isMeasureComplete(measure, s.id, learner.id)
-//
-//   def end(res: Map[String, Boolean]): Unit = {
-//      println(s"prontos. \n${args.toList}")
-//   }
-//}
+   }
+
+   def datasetFinished(ds: Ds) {
+   }
+
+   def isAlreadyDone(ds: Ds) = false //ds.isMeasureComplete(measure, s.id, learner.id)
+
+   def end(res: Map[String, Boolean]): Unit = {
+      println(s"prontos. \n${args.toList}")
+   }
+}
