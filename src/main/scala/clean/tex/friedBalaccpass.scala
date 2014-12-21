3d1673b92808a115ab63e16b63afe4dceb1962f8
davips <davips@users.noreply.github.com>
2014-12-21 11:09:40 -0200

    pus bug 3

--- src/main/scala/clean/tex/friedBalaccpass.scala
+++ src/main/scala/clean/tex/friedBalaccpass.scala
@@ -36,7 +36,7 @@ object friedBalaccpass extends AppWithUsage with LearnerTrait with StratsTrait w
    def ff(precision: Double)(x: Double) = (x * precision).round / precision
 
    override def run() = {
-      println(s"sou um bug     kjfhkcfjhcjvhb")
+      println(s"sou um bug     kjfhkcfjhcjvhbsdgfsdgfsdgsdg")
       //mudei tudo
       super.run()
 
