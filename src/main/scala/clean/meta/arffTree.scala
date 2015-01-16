6df9df150011eb1fb19fe269128eec83c7c00b78
davips <davips@users.noreply.github.com>
2015-01-16 20:32:22 -0200

    meta tree com empates (via repetição de exemplos)

--- src/main/scala/clean/meta/arffTree.scala
+++ src/main/scala/clean/meta/arffTree.scala
@@ -4,7 +4,7 @@ import java.io.FileWriter
 
 import clean._
 import clean.res.{ALCBalancedAcc, ALCKappa, BalancedAcc}
-import util.Stat
+import util.{StatTests, Stat}
 
 import scala.io.Source
 
@@ -27,10 +27,13 @@ import scala.io.Source
  */
 object arffTree extends AppWithUsage with StratsTrait with LearnerTrait with RangeGenerator {
    /*
-   Se colocar human=false, apenas um intervalo e apenas um learner,
-   dá na mesma que usar arffMeta.scala="Winner" com apenas um learner.
+   Antes, se colocasse human=false, apenas um intervalo e apenas um learner,
+   daria na mesma que usar arffMeta.scala="Winner" com apenas um learner.
+
+   Quando ties=true, o metaexemplo é repetido em caso de empate.
+   Caso contrário, apenas o melhor vencedor serve de rótulo.
     */
-   val humano = true
+   val ties = true
    val context = "metaAttsTreeApp"
    val arguments = superArguments
    //   val measure = ALCKappa
@@ -41,6 +44,7 @@ object arffTree extends AppWithUsage with StratsTrait with LearnerTrait with Ran
 
    override def run() = {
       super.run()
+      val ss = stratsForTree().map(_.abr).toVector
       val metadata0 = for {
          name <- datasets.toList
 
@@ -48,7 +52,7 @@ object arffTree extends AppWithUsage with StratsTrait with LearnerTrait with Ran
          (ti, tf, budix) <- {
             val ds = Ds(name, readOnly = true)
             ds.open()
-            val tmp = ranges(ds, 2, 100) // <- verificar!!! verificar tb argumentos do programa!!!
+            val tmp = ranges(ds, 2, 200) // <- verificar!!! verificar tb argumentos do programa!!!
             ds.close()
             tmp.zipWithIndex.map(x => (x._1._1, x._1._2, x._2))
          }
@@ -60,23 +64,41 @@ object arffTree extends AppWithUsage with StratsTrait with LearnerTrait with Ran
          val seqratts = (for (r <- 0 until Global.runs; f <- 0 until Global.folds) yield ds.attsFromR(r, f)).transpose.map(_.toVector)
          val rattsmd = seqratts map Stat.media_desvioPadrao
          val (rattsm, _) = rattsmd.unzip
-         val medidas = for {
-            s <- stratsForTree() // <- verificar!!!
-         } yield {
-            val le = if (s.id >= 17 && s.id <= 21 || s.id == 968 || s.id == 969) s.learner else l
-            val ms = for {
-               r <- 0 until Global.runs
-               f <- 0 until Global.folds
-            } yield measure(ds, s, le, r, f)(ti, tf).read(ds).getOrElse {
-                  ds.log(s" base incompleta para intervalo [$ti;$tf] e pool ${(s, le, r, f)}.", 40)
-                  -2d
-               }
-            s.abr -> Stat.media_desvioPadrao(ms.toVector)
-         }
-         val res = if (!humano) {
-            if (medidas.exists(x => x._2._1 == -2d)) None else Some(ds.metaAtts ++ rattsm, l.abr, medidas.maxBy(_._2._1)._1, if (budix == 0) "baixo" else "alto")
+         val res = if (ties) {
+            val vs = for {
+               r <- 0 until runs
+               f <- 0 until folds
+            } yield {
+               val poolStr = (100 * r + f).toString
+               val medidas = for {
+                  s <- stratsForTree() // <- verificar!!!
+                  le = if (s.id >= 17 && s.id <= 21 || s.id == 968 || s.id == 969) s.learner else l
+               } yield measure(ds, s, le, r, f)(ti, tf).read(ds).getOrElse {
+                     ds.log(s" base incompleta para intervalo [$ti;$tf] e pool ${(s, le, r, f)}.", 40)
+                     -2d
+                  }
+               poolStr -> medidas
+            }
+            val winners = StatTests.clearWinners(vs, ss)
+            ss.map { x =>
+               if (winners.contains(x)) Option(ds.metaAttsHumanAndKnowingLabels, l.abr, x, if (budix == 0) "baixo" else "alto")
+               else None
+            }.flatten
          } else {
-            if (medidas.exists(x => x._2._1 == -2d)) None else Some(ds.metaAttsHumanAndKnowingLabels, l.abr, medidas.maxBy(_._2._1)._1, if (budix == 0) "baixo" else "alto")
+            val medidas = for {
+               s <- stratsForTree() // <- verificar!!!
+            } yield {
+               val le = if (s.id >= 17 && s.id <= 21 || s.id == 968 || s.id == 969) s.learner else l
+               val ms = for {
+                  r <- 0 until Global.runs
+                  f <- 0 until Global.folds
+               } yield measure(ds, s, le, r, f)(ti, tf).read(ds).getOrElse {
+                     ds.log(s" base incompleta para intervalo [$ti;$tf] e pool ${(s, le, r, f)}.", 40)
+                     -2d
+                  }
+               s.abr -> Stat.media_desvioPadrao(ms.toVector)
+            }
+            if (medidas.exists(x => x._2._1 == -2d)) Seq() else Seq((ds.metaAttsHumanAndKnowingLabels, l.abr, medidas.maxBy(_._2._1)._1, if (budix == 0) "baixo" else "alto"))
          }
          ds.close()
          res
@@ -88,12 +110,12 @@ object arffTree extends AppWithUsage with StratsTrait with LearnerTrait with Ran
       val pred = metadata.map(_._3)
       val labels = pred.distinct.sorted
       val data = metadata.map { case (numericos, learner, vencedora, budget) => numericos.mkString(",") + s",$budget,$learner,$vencedora"}
-      val numAtts = if (!humano) nonHumanNumAttsNames else humanNumAttsNames
+      val numAtts = humanNumAttsNames
       val header = List("@relation data") ++ numAtts.split(",").map(i => s"@attribute $i numeric") ++ List("@attribute \"orçamento\" {baixo,alto}", "@attribute learner {" + allLearners().map(_.abr).mkString(",") + "}", "@attribute class {" + labels.mkString(",") + "}", "@data")
       val pronto = header ++ data
       pronto foreach println
 
-      val fw = new FileWriter("/home/davi/wcs/ucipp/uci/metaTree" + (if (humano) "Human" else "") + ".arff")
+      val fw = new FileWriter("/home/davi/wcs/ucipp/uci/metaTree" + (if (ties) "Ties" else "") + ".arff")
       pronto foreach (x => fw.write(s"$x\n"))
       fw.close()
       println(s"${data.size}")
