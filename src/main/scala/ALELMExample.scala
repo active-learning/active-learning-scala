1ef26dc68f128b18f2b11bbcfe045823812aae51
davips <davips@users.noreply.github.com>
2015-02-18 00:35:09 -0200

    CIBatch

--- src/main/scala/ALELMExample.scala
+++ src/main/scala/ALELMExample.scala
@@ -22,45 +22,44 @@ Copyright (C) 2014 Davi Pereira dos Santos
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 object ALELMExample extends App {
-  //  val patts0 = new Random(0).shuffle(Datasets.patternsFromSQLite("/home/davi/wcs/ucipp/uci")("gas-drift").right.get.take(1000000))
-  //  val patts0 = new Random(0).shuffle(Datasets.arff(true)("/home/davi/wcs/ucipp/uci/iris.arff").right.get.take(200000))
-  val patts0 = new Random(650).shuffle(Datasets.arff("/home/davi/wcs/ucipp/uci/abalone-11class.arff").right.get.take(2000))
-  val filter = Datasets.zscoreFilter(patts0)
-  val patts = Datasets.applyFilter(filter)(patts0)
+   val patts0 = new Random(0).shuffle(Datasets.arff("/home/davi/wcs/ucipp/uci/banana.arff").right.get.take(2000))
+   //  val patts0 = new Random(650).shuffle(Datasets.arff("/home/davi/wcs/ucipp/uci/abalone-11class.arff").right.get.take(2000))
+   val filter = Datasets.zscoreFilter(patts0)
+   val patts = Datasets.applyFilter(filter)(patts0)
 
-  val n = patts.length / 2
-  val initialN = patts.head.nclasses
-  // ClusterBased(patts.take(n))
-  val tri = DensityWeightedTrainingUtility(IELM(), patts.take(n), "eucl").queries
-  val tris = DensityWeightedTrainingUtility(IELMScratch(), patts.take(n), "eucl").queries
-  val trei = DensityWeightedTrainingUtility(EIELM(), patts.take(n), "eucl").queries
-  val trie = DensityWeightedTrainingUtility(IELMEnsemble(10), patts.take(n), "eucl").queries
-  val trci = DensityWeightedTrainingUtility(CIELM(), patts.take(n), "eucl").queries
-  val treci = DensityWeightedTrainingUtility(ECIELM(), patts.take(n), "eucl").queries
-  val ts = patts.drop(n)
+   val n = patts.length / 2
+   val initialN = patts.head.nclasses
+   // ClusterBased(patts.take(n))
+   val tri = DensityWeightedTrainingUtility(IELM(), patts.take(n), "eucl").queries
+   val tris = DensityWeightedTrainingUtility(IELMBatch(), patts.take(n), "eucl").queries
+   val trei = DensityWeightedTrainingUtility(EIELM(), patts.take(n), "eucl").queries
+   val trie = DensityWeightedTrainingUtility(IELMEnsemble(10), patts.take(n), "eucl").queries
+   val trci = DensityWeightedTrainingUtility(CIELM(), patts.take(n), "eucl").queries
+   val treci = DensityWeightedTrainingUtility(CIELMBatch(), patts.take(n), "eucl").queries
+   val ts = patts.drop(n)
 
-  val li = IELM()
-  val lis = IELMScratch()
-  val lei = EIELM()
-  val lie = IELMEnsemble(10)
-  val lci = CIELM()
-  val leci = ECIELM()
+   val li = IELM()
+   val lis = IELMBatch()
+   val lei = EIELM()
+   val lie = IELMEnsemble(10)
+   val lci = CIELM()
+   val leci = CIELMBatch()
 
-  var mi = li.build(tri.take(initialN))
-  var mis = lis.build(tris.take(initialN))
-  var mei = lei.build(trei.take(initialN))
-  var mie = lie.build(trie.take(initialN))
-  var mci = lci.build(trci.take(initialN))
-  var meci = leci.build(treci.take(initialN))
+   var mi = li.build(tri.take(initialN))
+   var mis = lis.build(tris.take(initialN))
+   var mei = lei.build(trei.take(initialN))
+   var mie = lie.build(trie.take(initialN))
+   var mci = lci.build(trci.take(initialN))
+   var meci = leci.build(treci.take(initialN))
 
-  val res = tri.zip(tris).zip(trei).zip(trie).zip(trci).zip(treci).drop(initialN).map { case (((((xi, xei), xis), xie), xci), xeci) =>
-    mi = li.update(mi)(xi)
-    mis = lis.update(mis)(xis)
-    mei = lei.update(mei)(xie)
-    mie = lie.update(mie)(xie)
-    mci = lci.update(mci)(xci)
-    meci = leci.update(meci)(xeci)
-    (mi.accuracy(ts), mis.accuracy(ts), mei.accuracy(ts), mie.accuracy(ts), mci.accuracy(ts), meci.accuracy(ts))
-  }
-  res foreach (x => println(s"${x._1} ${x._2} ${x._3} ${x._4} ${x._5} ${x._6}"))
+   val res = tri.zip(tris).zip(trei).zip(trie).zip(trci).zip(treci).drop(initialN).map { case (((((xi, xei), xis), xie), xci), xeci) =>
+      mi = li.update(mi)(xi)
+      mis = lis.update(mis)(xis)
+      mei = lei.update(mei)(xie)
+      mie = lie.update(mie)(xie)
+      mci = lci.update(mci)(xci)
+      meci = leci.update(meci)(xeci)
+      (mi.accuracy(ts), mis.accuracy(ts), mei.accuracy(ts), mie.accuracy(ts), mci.accuracy(ts), meci.accuracy(ts))
+   }
+   res foreach (x => println(s"${x._1} ${x._2} ${x._3} ${x._4} ${x._5} ${x._6}"))
 }
