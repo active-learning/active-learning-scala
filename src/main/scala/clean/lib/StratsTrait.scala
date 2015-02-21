33483d83ccaf3b7eb49cfdcb6921c9fe60d06c15
davips <davips@users.noreply.github.com>
2015-02-21 12:48:44 -0200

    16s 6l

--- src/main/scala/clean/lib/StratsTrait.scala
+++ src/main/scala/clean/lib/StratsTrait.scala
@@ -49,8 +49,9 @@ trait StratsTrait {
    )
 
    def stratsComLearnerExterno_FilterFree(pool: Seq[Pattern], learner: Learner) = List[Strategy](
-      Margin(learner, pool) //3
-      //      , DensityWeighted(learner, pool, 1, "eucl") //5
+      Entropy(learner, pool) //4
+      , Margin(learner, pool) //3
+      , DensityWeighted(learner, pool, 1, "eucl") //5
       , DensityWeightedTrainingUtility(learner, pool, "manh") //7
       , new SGmulti(learner, pool, "consensus") //14
       , ExpErrorReductionMargin(learner, pool, "balacc") //74
@@ -81,7 +82,7 @@ trait StratsTrait {
       RandomSampling(pool) //0
       , ClusterBased(pool) //1
       //      , Uncertainty(learner, pool) //2
-      //      , Entropy(learner, pool) //4
+      , Entropy(learner, pool) //4
       , Margin(learner, pool) //3
       //
       , ExpErrorReductionMargin(learner, pool, "entropy") //11
@@ -114,10 +115,12 @@ trait StratsTrait {
    def stratsForTreeRedux(pool: Seq[Pattern] = Seq(), learner: Learner = NoLearner()) = Seq(
       RandomSampling(pool) //0
       , ClusterBased(pool) //1
+      , Entropy(learner, pool) //4
       , Margin(learner, pool) //3
       , ExpErrorReductionMargin(learner, pool, "entropy") //11
       , ExpErrorReductionMargin(learner, pool, "balacc") //74
       , new SGmulti(learner, pool, "consensus") //14
+      , DensityWeighted(learner, pool, 1, "eucl") //5
       , DensityWeightedTrainingUtility(learner, pool, "manh") //7
       , AgDensityWeightedTrainingUtility(pool, "manh") //701
       , DensityWeightedTrainingUtility(learner, pool, "maha") //9
