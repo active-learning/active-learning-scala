6825f6b9c752f3b9844d7c587ae2ab33ee80a7c8
davips <davips@users.noreply.github.com>
2015-03-31 01:26:05 -0300

    mem sh

--- m.sh
+++ m.sh
@@ -4,6 +4,9 @@ while true; do
     if [ "$1" -ge "$available" ]; then
 	echo "memoria quase cheia:$available ; matando "
 	kill $(ps aux --sort=-%mem | awk 'NR<=10{print $0}' | grep java | grep davi | head -1 | tr -s ' ' | cut -d' ' -f2)
+	
+	echo "criando outro java"
+	 p="n"; cd ~/wcs/als/; java -Xmx100000m -cp /home/davi/wcs/als/target/scala-2.10/als-assembly-a.jar clean.run.acv 10 q $p 200 &> /dev/null &
     fi
     sleep 10
 done
