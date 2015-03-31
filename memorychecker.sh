56f1ce8270e4465482f80d75f5c64002180e9b46
davips <davips@users.noreply.github.com>
2015-03-31 00:28:32 -0300

    mem

--- memorychecker.sh
+++ memorychecker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 threshold=2000          # Minimum amount of memory left when you should start killing, in MB
 while true; do
-    available=$(free -m | head -2 | tail -1 | awk '{print $4}')
+    available=$(free -m | head -3 | tail -1 | awk '{print $4}')
     if [ "$threshold" -ge "$available" ]; then
 	echo "memoria quase cheia:$available"
 	/home/davi/wcs/als/memorykiller.sh
