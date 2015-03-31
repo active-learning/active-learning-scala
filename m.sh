3d83a7e3cd1026a620ad7abf8731056d73fc6392
davips <davips@users.noreply.github.com>
2015-03-31 01:27:23 -0300

    mem sh

--- m.sh
+++ m.sh
@@ -6,7 +6,7 @@ while true; do
 	kill $(ps aux --sort=-%mem | awk 'NR<=10{print $0}' | grep java | grep davi | head -1 | tr -s ' ' | cut -d' ' -f2)
 	
 	echo "criando outro java"
-	 p="n"; cd ~/wcs/als/; java -Xmx100000m -cp /home/davi/wcs/als/target/scala-2.10/als-assembly-a.jar clean.run.acv 10 q $p 200 &> /dev/null &
+	cd ~/wcs/als/; java -Xmx100000m -cp /home/davi/wcs/als/target/scala-2.10/als-assembly-a.jar clean.run.acv 10 q $2 200 &> /dev/null &
     fi
     sleep 10
 done
