52ef3747cb5c0b77f1390a71e08fd91c362cd913
davips <davips@users.noreply.github.com>
2015-04-07 04:05:23 -0300

    euc

--- src/main/scala/clean/tex/tabwinners.scala
+++ src/main/scala/clean/tex/tabwinners.scala
@@ -29,7 +29,7 @@ object tabwinners extends AppWithUsage with LearnerTrait with StratsTrait with R
    //, "comprimento:all,half,50", "porRisco:r", "dist:euc,man,mah")
    val context = "tabwinnerstex"
    val n = 1
-   val dista = "man"
+   val dista = "euc"
    run()
 
    override def run() = {
