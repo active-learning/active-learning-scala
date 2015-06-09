924ec15fbbde6f9c3439e1940cc65d7e8fb05ee9
davips <davips@users.noreply.github.com>
2015-06-09 16:32:36 -0300

    testa todas strats variando o lea [1x2-fold rf5 e outras alteracoes temporarias]

--- src/main/scala/clean/tex/metaParesByPool.scala
+++ src/main/scala/clean/tex/metaParesByPool.scala
@@ -48,129 +48,112 @@ object metaParesByPool extends AppWithUsage with LearnerTrait with StratsTrait w
   val featureSel = false
 
   val (ini, fim) = ("ti", "tf")
-  val (rus, ks) = 1 -> 94
+  val (rus, ks) = 1 -> 2
   run()
 
   override def run() = {
     super.run()
     val ls = learners(learnersStr)
-    //        val ss = stratsTexRedux("eucl")
-    //        val ss = stratsTexRedux("all")
-    //        val ss = stratsTex("all")
-    //    val ss = Seq((l: Learner) => MarginFixo(l, Seq()), (l: Learner) => ExpErrorReductionMarginFixo(l, Seq(), "entropy"))//, (l: Learner) => HTUFixo(Seq(), l, Seq(), "maha"))
-    //    val ss = Seq((l: Learner) => MarginFixo(l, Seq()))
-    //        val ss = Seq(
-    //          (l: Learner) => MarginFixo(l, Seq()),
-    //          (l: Learner) => ExpErrorReductionMarginFixo(l, Seq(), "entropy")
-    //        )
-    //    val ss = Seq((l: Learner) => ExpErrorReductionMarginFixo(l, Seq(), "entropy"))
-    val ss = Seq((l: Learner) => HTUFixo(Seq(), l, Seq(), "maha"))
-    //    val ss = Seq((l: Learner) => HTUFixo(Seq(), l, Seq(), "eucl"), (l: Learner) => HTUFixo(Seq(), l, Seq(), "manh"), (l: Learner) => HTUFixo(Seq(), l, Seq(), "maha"))
-    //      //          DensityWeightedTrainingUtilityFixo(Seq(), RF(), Seq(), "eucl")
-    //    val ss = Seq((l: Learner) => AgDensityWeightedTrainingUtility(Seq(), "eucl"), (l: Learner) => AgDensityWeightedTrainingUtility(Seq(), "manh"), (l: Learner) => AgDensityWeightedTrainingUtility(Seq(), "maha"))
-    //      //      ClusterBased(Seq()),
-    //      //      RandomSampling(Seq())
-    //    )
-    val pares = for {l <- ls; s <- ss} yield s -> l
-    //    $rus.$ks
-    val arq = s"/home/davi/wcs/arff/$context-n${if (porRank) 1 else n}best${melhor}m$measure-$ini.$fim-${pares.map { case (s, l) => s(l).id }.mkString("-").hashCode.toLong.abs + (if (porRank) "Rank" else "")}-${learnerStr.replace(" ", ".")}-p$dsminSize.arff"
-    val labels = pares.map { case (s, l) => s(l).limpa }
-
-    //cada dataset produz um bag de metaexemplos (|bag| >= 25)
-    def bagsNaN = DsByMinSize(datasets, dsminSize).par map { d =>
-      val ds = Ds(d, readOnly = true)
-      ds.open()
-      val (ti0, th0, tf0, tpass) = ranges(ds)
-      val ti = if (ini == "ti") ti0 else th0
-      val tf = if (fim == "th") th0 else tf0
-      val res = for {
-        r <- 0 until runs
-        f <- 0 until folds
-      } yield {
-          //descobre vencedores deste pool
-          val accs = pares map { case (s, l) =>
-
-            //            p -> measure(ds, Passive(Seq()), ds.bestPassiveLearner, r, f)(ti,tf).read(ds).getOrElse(error("sem medida"))
-
-            s(l) -> measure(ds, s(l), l, r, f)(ti, tf).read(ds).getOrElse(error("sem medida"))
-
-            //            val classif = BestClassifCV100_10foldReadOnlyKappa(ds, r, f, s(l))
-            //            s(l) -> measure(ds, s(l), classif, r, f)(-2).read(ds).getOrElse(error("sem medida"))
-
-          }
-          //gera metaexemplos
-          val metaatts0 = ds.metaAttsrf(r, f).map(x => (x._1, x._2.toString, x._3)) ++ ds.metaAttsFromR(r, f).map(x => (x._1, x._2.toString, x._3))
-          val metaatts = ("\"bag_" + pares.size + "\"", ds.dataset, "string") +: metaatts0
-          if (porRank) {
-            //rank legivel por clus e ELM
-            List(metaatts ++ ranqueia(accs.map(_._2)).zipWithIndex.map { case (x, i) => (s"class$i", x.toString, "numeric") } -> "")
-          } else {
-            //Acc
-            val melhores = pegaMelhores(accs, n)(_._2 * melhor).map(_._1)
-            melhores map (m => metaatts -> m.limpa)
-          }
-        }
-      ds.close()
-      res.flatten
-    }
-    def bags = bagsNaN
-    println(s"$arq")
-    if (!new File(arq).exists()) grava(arq, arff(labels.mkString(","), bags.toList.flatten, print = true, context, porRank))
-    println(s"$arq")
-
-    val patterns = Datasets.arff(arq, dedup) match {
-      case Right(x) => x
-      case Left(m) => error(s"${m} <- m")
-    }
-
-    println(s"${patterns.size} <- patterns.size")
-
-    // refaz bags por base
     val metaclassifs = (patts: Vector[Pattern]) => if (porRank) Vector()
     else Vector(//NB não funciona porque quebra na discretização
       CIELMBatch(),
       C45(false, 5),
-      C45(false, 25),
+      //      C45(false, 25),
       KNNBatcha(5, "eucl", patts),
-      KNNBatcha(25, "eucl", patts),
-      RF(42, 500),
-      SVMLibRBF(),
+      //      KNNBatcha(25, "eucl", patts),
+      RF(42, 5),
+      //      SVMLibRBF(),
       NinteraELM(),
       Maj())
+    stratsTex("all") foreach { strat =>
+      val pares = for {l <- ls} yield strat -> l
+      //    $rus.$ks
+      val arq = s"/home/davi/wcs/arff/$context-n${if (porRank) 1 else n}best${melhor}m$measure-$ini.$fim-${pares.map { case (s, l) => s(l).id }.mkString("-").hashCode.toLong.abs + (if (porRank) "Rank" else "")}-${learnerStr.replace(" ", ".")}-p$dsminSize.arff"
+      val labels = pares.map { case (s, l) => s(l).limpa }
 
-    Tempo.start
-    println()
-    val porMetaLea = cv(featureSel, patterns, metaclassifs, porRank, rus, ks).toVector.flatten.flatten.groupBy(_.metalearner)
-    def fo(x: Double) = "%2.3f".format(x)
+      //cada dataset produz um bag de metaexemplos (|bag| >= 25)
+      def bagsNaN = DsByMinSize(datasets, dsminSize).par map { d =>
+        val ds = Ds(d, readOnly = true)
+        ds.open()
+        val (ti0, th0, tf0, tpass) = ranges(ds)
+        val ti = if (ini == "ti") ti0 else th0
+        val tf = if (fim == "th") th0 else tf0
+        val res = for {
+          r <- 0 until runs
+          f <- 0 until folds
+        } yield {
+            //descobre vencedores deste pool
+            val accs = pares map { case (s, l) =>
 
-    if (porRank) println(s"Pearson correl.: higher is better. $rus*$ks-fold CV. $measure$ini-$fim${if (featureSel) "FeatSel" else ""}")
-    else println(s"Accuracy: higher is better. $rus*$ks-fold CV. $n best. $measure$ini-$fim${if (featureSel) "FeatSel" else ""}")
+              //            p -> measure(ds, Passive(Seq()), ds.bestPassiveLearner, r, f)(ti,tf).read(ds).getOrElse(error("sem medida"))
 
-    porMetaLea foreach { case (nome, resultados) =>
-      val accTr = Stat.media_desvioPadrao(resultados.map(_.accTr))
-      val accTs = Stat.media_desvioPadrao(resultados.map(_.accTs))
-      println(s"$nome:\t${fo(accTr._1)}/${fo(accTr._2)}\t${fo(accTs._1)}/${fo(accTs._2)}")
-    }
+              s(l) -> measure(ds, s(l), l, r, f)(ti, tf).read(ds).getOrElse(error("sem medida"))
 
-    println()
-    println("histogramas ===========================")
-    porMetaLea foreach { case (nome, resultados) =>
-      val r = resultados reduce (_ ++ _)
-      println(s"$nome: ------------")
-      println(s"treino")
-      r.histTr.padTo(6, "   ").zip(r.histTrPred.padTo(6, "   ")).map(x => x._1 + "\t\t" + x._2).take(6) foreach println
-      println(s"teste")
-      r.histTs.padTo(6, "   ").zip(r.histTsPred.padTo(6, "   ")).map(x => x._1 + "\t\t" + x._2).take(6) foreach println
+              //            val classif = BestClassifCV100_10foldReadOnlyKappa(ds, r, f, s(l))
+              //            s(l) -> measure(ds, s(l), classif, r, f)(-2).read(ds).getOrElse(error("sem medida"))
+
+            }
+            //gera metaexemplos
+            val metaatts0 = ds.metaAttsrf(r, f).map(x => (x._1, x._2.toString, x._3)) ++ ds.metaAttsFromR(r, f).map(x => (x._1, x._2.toString, x._3))
+            val metaatts = ("\"bag_" + pares.size + "\"", ds.dataset, "string") +: metaatts0
+            if (porRank) {
+              //rank legivel por clus e ELM
+              List(metaatts ++ ranqueia(accs.map(_._2)).zipWithIndex.map { case (x, i) => (s"class$i", x.toString, "numeric") } -> "")
+            } else {
+              //Acc
+              val melhores = pegaMelhores(accs, n)(_._2 * melhor).map(_._1)
+              melhores map (m => metaatts -> m.limpa)
+            }
+          }
+        ds.close()
+        res.flatten
+      }
+      def bags = bagsNaN
+      println(s"$arq")
+      if (!new File(arq).exists()) grava(arq, arff(labels.mkString(","), bags.toList.flatten, print = true, context, porRank))
+      println(s"$arq")
+
+      val patterns = Datasets.arff(arq, dedup) match {
+        case Right(x) => x
+        case Left(m) => error(s"${m} <- m")
+      }
+
+
+      Tempo.start
+      println(s"${patterns.size} <- patterns.size")
       println()
-    }
+      val porMetaLea = cv(featureSel, patterns, metaclassifs, porRank, rus, ks).toVector.flatten.flatten.groupBy(_.metalearner)
+      def fo(x: Double) = "%2.3f".format(x)
 
-    println()
-    println("resultados ===========================")
-    porMetaLea foreach { case (nome, resultados) =>
-      val accTr = Stat.media_desvioPadrao(resultados.map(_.accTr))
-      val accTs = Stat.media_desvioPadrao(resultados.map(_.accTs))
-      println(s"$nome\t:\t${fo(accTr._1)}/${fo(accTr._2)}\t${fo(accTs._1)}/${fo(accTs._2)}")
+      if (porRank) println(s"Pearson correl.: higher is better. $rus*$ks-fold CV. $measure$ini-$fim${if (featureSel) "FeatSel" else ""}")
+      else println(s"Accuracy: higher is better. $rus*$ks-fold CV. $n best. $measure$ini-$fim${if (featureSel) "FeatSel" else ""}")
+
+      porMetaLea foreach { case (nome, resultados) =>
+        val accTr = Stat.media_desvioPadrao(resultados.map(_.accTr))
+        val accTs = Stat.media_desvioPadrao(resultados.map(_.accTs))
+        println(s"$nome:\t${fo(accTr._1)}/${fo(accTr._2)}\t${fo(accTs._1)}/${fo(accTs._2)}")
+      }
+
+      println()
+      println("histogramas ===========================")
+      porMetaLea foreach { case (nome, resultados) =>
+        val r = resultados reduce (_ ++ _)
+        println(s"$nome: ------------")
+        println(s"treino")
+        r.histTr.padTo(6, "   ").zip(r.histTrPred.padTo(6, "   ")).map(x => x._1 + "\t\t" + x._2).take(6) foreach println
+        println(s"teste")
+        r.histTs.padTo(6, "   ").zip(r.histTsPred.padTo(6, "   ")).map(x => x._1 + "\t\t" + x._2).take(6) foreach println
+        println()
+      }
+
+      println()
+      println("resultados ===========================")
+      porMetaLea foreach { case (nome, resultados) =>
+        val accTr = Stat.media_desvioPadrao(resultados.map(_.accTr))
+        val accTs = Stat.media_desvioPadrao(resultados.map(_.accTs))
+        println(s"$nome\t:\t${fo(accTr._1)}/${fo(accTr._2)}\t${fo(accTs._1)}/${fo(accTs._2)}")
+      }
+      Tempo.print_stop
     }
-    Tempo.print_stop
   }
 }
