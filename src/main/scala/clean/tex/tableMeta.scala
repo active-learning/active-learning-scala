2d2ad90a89185208978743dc61d42496bfb9472b
davips <davips@users.noreply.github.com>
2015-06-27 14:27:55 -0300

    rank tab

--- src/main/scala/clean/tex/tableMeta.scala
+++ src/main/scala/clean/tex/tableMeta.scala
@@ -10,18 +10,20 @@ import util.{StatTests, Datasets, Stat, Tempo}
 import scala.io.Source
 
 object tableMeta extends App {
+  val acc = args.head == "ac"
   //PE tá treinando!
   //    val sortedMCs= List("ELM","ELMr-a","PCT","PCTr-a","PE","PEr-a","RFw","C4.5","maj")
-  val sortedMCs = List("PCT+ ELM", "ELM", "PCT", "RFw", "C4.5w", "5NN", "maj") //maj tem que ser o último!
+  val sortedMCs = if (acc) List("PCT+ ELM", "ELM", "PCT", "RFw", "C4.5w", "5NN", "maj") //maj tem que ser o último!
+  else List("PCT+ ELM", "ELM", "PCT", "def")
 
   def ord2(l: List[Vector[String]]) = {
     val u = sortedMCs.zipWithIndex.sortBy(_._1)
     l.sortBy(_(1)).zip(u).sortBy(_._2._2).map(_._1)
   }
 
-  val db = new Db("meta", true)
+  val db = new Db("meta", true) //true=faster
   db.open()
-  val dbrows = db.readString("select st,mc,abts,dbts,htr from r where rs=10 AND fs=10 and nt=1000 and (sm like 'nosm%') and mc in (" + sortedMCs.map {
+  val dbrows = if (acc) db.readString("select st,mc,abts,dbts,htr from r where rs=10 AND fs=10 and nt=1000 and (sm like 'nosm%') and mc in (" + sortedMCs.map {
     case "PCT+ ELM" => "'PEr-a'"
     case "PCT" => "'PCTr-a'"
     case "ELM" => "'ELMr-a'"
@@ -29,25 +31,36 @@ object tableMeta extends App {
     case "C4.5w" => "'C4.55'"
     case x => s"'$x'"
   }.mkString(",") + ") and fsel not like 'pca%' order by abts;")
+  else db.readString("select st,mc,ats,dts,htr from r where rs=10 AND fs=10 and nt=1000 and (sm like 'nosm%') and mc in (" + sortedMCs.map {
+    case "PCT+ ELM" => "'PEr'"
+    case "PCT" => "'PCTr'"
+    case "ELM" => "'ELMr'"
+    case "def" => "'defr'"
+    case x => s"'$x'"
+  }.mkString(",") + ") and fsel not like 'pca%' order by abts;")
+
   //  val tableheader = "\t& " + (sortedMCs ++ List("$\\delta$", "$\\frac{\\maj}{\\min}$")).mkString(" & ") + " \\\\ \\hline"
-  val tableheader = "\t& " + (sortedMCs ++ List("$\\delta$")).mkString(" & ") + " \\\\ \\hline"
+  val tableheader = "\t& " + (sortedMCs).mkString(" & ") + " \\\\ \\hline"
   val tablerows0 = dbrows.groupBy(_.head).values.toList.sortBy(_.head.head).map { case veclst =>
     val strat = veclst.head.head
-    val Min = (veclst map { case Vector(st, metacla, acc, dev, _) => acc }).dropRight(1).min //maj nao interessa
+    val vord = ord2(veclst) map { case Vector(st, metacla, acc, dev, _) => acc }
+    val Min = vord.dropRight(1).min //maj nao interessa
+  lazy val maj = vord.last.toDouble
     val Max = (veclst map { case Vector(st, metacla, acc, dev, _) => acc }).max
-    val strs = (ord2(veclst) map {
+    val strs = ord2(veclst) map {
       case Vector(st, metacla, Max, dev, _) => s"\\textcolor{blue}{\\textbf{${"%2d".format((Max.toDouble * 100).round)}}}/${"%2d".format((dev.toDouble * 100).round)}";
       case Vector(st, metacla, Min, dev, _) => s"\\textcolor{red}{${"%2d".format((Min.toDouble * 100).round)}}/${"%2d".format((dev.toDouble * 100).round)}";
       case Vector(st, metacla, acc, dev, _) => s"${"%2d".format((acc.toDouble * 100).round)}/${"%2d".format((dev.toDouble * 100).round)}";
       case x => db.error(s"${x} <- x ALERTA")
-    }) ++ List(s"${"%2d".format(((Min.toDouble - ord2(veclst).head.reverse.tail.head.toDouble) * 100).round)}")
+    } // ++ List(s"${"%2d".format(((Min.toDouble - maj) * 100).round)}")
     //    val hx = veclst.head(4).split(";").head.split(" ").map(_.toDouble).max
     //    val hn = veclst.head(4).split(";").head.split(" ").map(_.toDouble).min
     strat + "\t& " + strs.mkString(" & ")
     //    + " & " + "%2d".format((100 * hx / hn).round) //max/min
   }
-  val tablerows = tablerows0.sortBy(_.split("&")(sortedMCs.size + 1).split("\\\\\\\\").head.toDouble).reverse.mkString(" \\\\ \n")
-  println(s"\\begin{table}[h]\n\\begin{center}\n\\begin{tabular}{l|b{1cm}" + Seq.fill(tableheader.split("&").size - 2)("r").mkString + "}")
+  //  tablerows0 foreach println
+  val tablerows = tablerows0.sortBy(_.split("&")(sortedMCs.size).split("/").head.toDouble).reverse.mkString(" \\\\ \n")
+  println(s"\\begin{table}[h]\n\\begin{center}\n\\begin{tabular}{l|l" + Seq.fill(tableheader.split("&").size - 2)("l").mkString + "}")
   println(s"${tableheader}")
   println(s"${tablerows}")
   println("\\end{tabular}\n\\end{center}\n\\end{table}")
