2b2853e346bee378ee75c28ea0a1aae2df85f6cd
davips <davips@users.noreply.github.com>
2015-06-27 13:11:33 -0300

    fried meta

--- src/main/scala/clean/tex/tableMeta.scala
+++ src/main/scala/clean/tex/tableMeta.scala
@@ -5,44 +5,66 @@ import java.io.{File, FileWriter}
 import clean.lib._
 import ml.Pattern
 import ml.classifiers._
-import util.{Datasets, Stat, Tempo}
+import util.{StatTests, Datasets, Stat, Tempo}
 
 import scala.io.Source
 
 object tableMeta extends App {
-  //PE não aparece!
-  //  val ord = List("ELM","ELMr-a","PCT","PCTr-a","PE","PEr-a","RFw1000","C4.55","maj")
-  val ord = List("ELM", "ELMr-a", "PCT", "PCTr-a", "PEr-a", "RFw1000", "C4.55", "maj")
+  //PE tá treinando!
+  //    val sortedMCs= List("ELM","ELMr-a","PCT","PCTr-a","PE","PEr-a","RFw","C4.5","maj")
+  val sortedMCs = List("PCT+ ELM", "ELM", "PCT", "RFw", "C4.5w", "5NN", "maj") //maj tem que ser o último!
 
   def ord2(l: List[Vector[String]]) = {
-    val u = ord.zipWithIndex.sortBy(_._1)
+    val u = sortedMCs.zipWithIndex.sortBy(_._1)
     l.sortBy(_(1)).zip(u).sortBy(_._2._2).map(_._1)
   }
 
   val db = new Db("meta", true)
   db.open()
-  val dbrows = db.readString("select st,mc,abts,dbts,htr from r where rs=10 AND fs=10 and nt=1000 and (sm like 'nosm%') and mc in ('ELM','ELMr-a','PCT','PCTr-a','PE','PEr-a','RFw1000','C4.55','maj') and fsel not like 'pca%' order by abts;")
-  //  val tableheader = "\t& " + ord(dbrows.groupBy(_.head).values.flatten.map(_(1)).toList.distinct).mkString(" & ") + " \\\\ \\hline"
-  val tableheader = "\t& " + (ord ++ List("$\\delta$", "$\\frac{\\maj}{\\min}$")).mkString(" & ") + " \\\\ \\hline"
-  val tablerows = dbrows.groupBy(_.head).values.toList.sortBy(_.head.head).map { case veclst =>
+  val dbrows = db.readString("select st,mc,abts,dbts,htr from r where rs=10 AND fs=10 and nt=1000 and (sm like 'nosm%') and mc in (" + sortedMCs.map {
+    case "PCT+ ELM" => "'PEr-a'"
+    case "PCT" => "'PCTr-a'"
+    case "ELM" => "'ELMr-a'"
+    case "RFw" => "'RFw1000'"
+    case "C4.5w" => "'C4.55'"
+    case x => s"'$x'"
+  }.mkString(",") + ") and fsel not like 'pca%' order by abts;")
+  //  val tableheader = "\t& " + (sortedMCs ++ List("$\\delta$", "$\\frac{\\maj}{\\min}$")).mkString(" & ") + " \\\\ \\hline"
+  val tableheader = "\t& " + (sortedMCs ++ List("$\\delta$")).mkString(" & ") + " \\\\ \\hline"
+  val tablerows0 = dbrows.groupBy(_.head).values.toList.sortBy(_.head.head).map { case veclst =>
     val strat = veclst.head.head
-    val Min = (veclst map { case Vector(st, metacla, acc, dev, _) => acc }).min
+    val Min = (veclst map { case Vector(st, metacla, acc, dev, _) => acc }).dropRight(1).min //maj nao interessa
     val Max = (veclst map { case Vector(st, metacla, acc, dev, _) => acc }).max
     val strs = (ord2(veclst) map {
       case Vector(st, metacla, Max, dev, _) => s"\\textcolor{blue}{\\textbf{${"%2d".format((Max.toDouble * 100).round)}}}/${"%2d".format((dev.toDouble * 100).round)}";
       case Vector(st, metacla, Min, dev, _) => s"\\textcolor{red}{${"%2d".format((Min.toDouble * 100).round)}}/${"%2d".format((dev.toDouble * 100).round)}";
       case Vector(st, metacla, acc, dev, _) => s"${"%2d".format((acc.toDouble * 100).round)}/${"%2d".format((dev.toDouble * 100).round)}";
       case x => db.error(s"${x} <- x ALERTA")
-    }) ++ List(s"${"%2d".format(((Max.toDouble - Min.toDouble) * 100).round)}")
-    val hx = veclst.head(4).split(";").head.split(" ").map(_.toDouble).max
-    val hn = veclst.head(4).split(";").head.split(" ").map(_.toDouble).min
-    strat + "\t& " + strs.mkString(" & ") + " & " + "%2d".format((100 * hx / hn).round)
-  }.sortBy(_.split("&")(ord.size).split("\\\\\\\\").head.toDouble).reverse.mkString(" \\\\ \n")
-  println(s"\\begin{table}[h]\n\\begin{center}\n\\begin{tabular}{l|" + Seq.fill(tableheader.split("&").size - 1)("r").mkString + "}")
+    }) ++ List(s"${"%2d".format(((Min.toDouble - ord2(veclst).head.reverse.tail.head.toDouble) * 100).round)}")
+    //    val hx = veclst.head(4).split(";").head.split(" ").map(_.toDouble).max
+    //    val hn = veclst.head(4).split(";").head.split(" ").map(_.toDouble).min
+    strat + "\t& " + strs.mkString(" & ")
+    //    + " & " + "%2d".format((100 * hx / hn).round) //max/min
+  }
+  val tablerows = tablerows0.sortBy(_.split("&")(sortedMCs.size + 1).split("\\\\\\\\").head.toDouble).reverse.mkString(" \\\\ \n")
+  println(s"\\begin{table}[h]\n\\begin{center}\n\\begin{tabular}{l|b{1cm}" + Seq.fill(tableheader.split("&").size - 2)("r").mkString + "}")
   println(s"${tableheader}")
   println(s"${tablerows}")
   println("\\end{tabular}\n\\end{center}\n\\end{table}")
   db.close()
+
+
+  val tab = dbrows.groupBy(_.head).values.toList.sortBy(_.head.head).map { case veclst =>
+    val strat = veclst.head.head
+    val vals = ord2(veclst) map {
+      case Vector(st, metacla, acc, dev, _) => acc.toDouble
+      case x => db.error(s"${x} <- x ALERTA")
+    }
+    strat -> vals
+  }
+  val pairs = StatTests.friedmanNemenyi(tab, sortedMCs.toVector)
+  val fri = StatTests.pairTable(pairs, "stratsfriedpares", 2, "fried")
+  println(s"${fri}")
 }
 
 /*
