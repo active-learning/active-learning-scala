ee87c688d1a52bfe6a43720c99ee3183dcd051b5
davips <davips@users.noreply.github.com>
2015-09-21 16:07:49 -0300

    split

--- src/main/scala/clean/tex/plotPares.scala
+++ src/main/scala/clean/tex/plotPares.scala
@@ -94,8 +94,7 @@ object plotPares extends AppWithUsage with LearnerTrait with StratsTrait with Ra
       x.sliding(20).map(y => y.sum / y.size).toList
     }.transpose
 
-    val porLea = algs.zip(plot.transpose).groupBy(_._1).toList
-    //    val porLea = algs.zip(plot.transpose).groupBy(_._1.split("-").last).toList
+    val porLea = algs.zip(plot.transpose).groupBy(_._1.replace("-a", "+a").split("-").last).toList
 
     val minMaxBest = porLea map { case (lea, curvas) =>
       val min = "min" + lea -> curvas.map(_._2).transpose.map(_.min)
