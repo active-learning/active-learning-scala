6b3e4b2cfcfc3fc0a46f015fc209292147b4abec
davips <davips@users.noreply.github.com>
2015-09-27 16:54:14 -0300

    descs

--- src/main/scala/clean/tex/datasetsdesc.scala
+++ src/main/scala/clean/tex/datasetsdesc.scala
@@ -45,28 +45,32 @@ object datasetsdesc extends Exp with Lock {
 
   def datasetFinished(ds: Ds) {
     acquire()
-    println(ds.description)
-    m += renomeia(ds) -> (ds.description._1.map(x => x.toString)) // ++ ds.description._2.dropRight(1).map(x => "%5.1f".format(x)))
+    //    println(ds.description)
+    if (ds.poolSize > 200) println(s"$ds")
+    m += renomeia(ds) -> (ds.description._1.map(x => x.toString) ++ ds.description._2.dropRight(1).map(x => "%5.1f".format(x)))
     //    if (ds.nclasses == 2 && ds.poolSize > 200) m += renomeia(ds) -> (ds.description2._1.map(x => x.toString)) // ++ ds.description._2.dropRight(1).map(x => "%5.1f".format(x)))
-    //    if (ds.nclasses == 2) m += renomeia(ds) -> (ds.description._1.map(_.toString)) ++ ds.description._2.dropRight(1).map(x => "%5.1f".format(x)))
+    //        if (ds.nclasses == 2) m += renomeia(ds) -> (ds.description._1.map(_.toString)) ++ ds.description._2.dropRight(1).map(x => "%5.1f".format(x)))
     release()
   }
 
-  lazy val descriptionNames = Seq(
-    """\pbox{20cm}{\#exemplos\\($|\mathcal{U}|$)}""",
-    """\pbox{20cm}{\#classes\\($|Y|$)}""",
-    "\\#atributos",
-    "\\#nominais",
-    "")
-  //  lazy val descriptionNames = Seq( """\pbox{20cm}{\#exemplos\\($|\mathcal{U}|$)}""", """\pbox{20cm}{\#classes\\($|Y|$)}""", "\\#atributos", "\\#nominais", "\\%majoritária", "\\%minoritária", """\pbox{20cm}{entropia da \\distr. de classes}""")
+  //  lazy val descriptionNames = Seq(
+  //    """\pbox{20cm}{\#exemplos\\($|\mathcal{U}|$)}""",
+  //    """\pbox{20cm}{\#classes\\($|Y|$)}""",
+  //    "\\#atributos",
+  //    "\\#nominais",
+  //    "")
+  lazy val descriptionNames = Seq( """\pbox{20cm}{\#exemplos\\($|\mathcal{U}|$)}""", """\pbox{20cm}{\#classes\\($|Y|$)}""", "\\#atributos", "\\#nominais", "\\%majoritária", "\\%minoritária") //, """\pbox{20cm}{entropia da \\distr. de classes}""")
+
+  lazy val header = " & \\rotatebox{0}{$|\\mathcal{U}|$} & \\rotatebox{0}{$|Y|$} & \\rotatebox{0}{atributos} & \\rotatebox{0}{nominais} & \\rotatebox{0}{\\makecell{majoritária\\\\(\\%)}} & \\rotatebox{0}{\\makecell{minoritária\\\\(\\%)}}\\\\ \\hline"
 
   def tabela(tableName: String, caption: String, core: Seq[(String, Seq[String])]) = {
-    val cols = descriptionNames.size - 1
+    val cols = descriptionNames.size
     """
 \begin{table}[h]
 \caption{""" + caption + """}
 \begin{center}
-\begin{tabular}{l|""" + Seq.fill(cols)("r").mkString(" ") + "}\n & " + descriptionNames.dropRight(1).map(x => "\\rotatebox{0}{" + x + "}").mkString(" & ") + "\\\\\n" + """ \hline """ + "\n" +
+\begin{tabular}{l|""" + Seq.fill(cols)("r").mkString(" ") + "}\n " + header + "\n" +
+      //\begin{tabular}{l|""" + Seq.fill(cols)("r").mkString(" ") + "}\n & " + descriptionNames.map(x => "\\rotatebox{0}{" + x + "}").mkString(" & ") + "\\\\\n" + """ \hline """ + "\n" +
       core.zipWithIndex.map { case (x, i) => (x._1 +: x._2).mkString(" & ") + "\\\\" + (if (i % 3 == 2) "\\hline" else "") }.mkString("\n") +
       """
 \end{tabular}
@@ -80,30 +84,30 @@ object datasetsdesc extends Exp with Lock {
       ((i + 1) + "-" + d) -> l
     }
     println(todas.size + " " + datasets.size)
-    val fw2 = new PrintWriter("/home/davi/wcs/artigos/revista15/dataset-tables.tex", "UTF-8")
-    fw2.write(tabela("tab:datasetsa", "Características das bases de dados (1-33).", todas.take(47)))
-    fw2.write(tabela("tab:datasetsb", "Características das bases de dados (34-66).", todas.drop(47)))
+    //    val fw2 = new PrintWriter("/home/davi/wcs/artigos/revista15/dataset-tables.tex", "UTF-8")
+    //    fw2.write(tabela("tab:datasetsa", "Características das bases de dados (1-33).", todas.take(47)))
+    //    fw2.write(tabela("tab:datasetsb", "Características das bases de dados (34-66).", todas.drop(47)))
 
-    //    fw2.write(tabela("tab:datasetsa", "Características das bases de dados (1-33).", todas.take(33)))
-    //    fw2.write(tabela("tab:datasetsb", "Características das bases de dados (34-66).", todas.drop(33).take(33)))
-    //    fw2.write(tabela("tab:datasetsc", "Características das bases de dados (67-94).", todas.drop(66)))
+    val fw2 = new PrintWriter("/home/davi/wcs/tese/dataset-tables.tex", "UTF-8")
+    fw2.write(tabela("tab:datasetsa", "Características das bases de dados (1-38).", todas.take(38)))
+    fw2.write(tabela("tab:datasetsb", "Características das bases de dados (39-75).", todas.drop(38)))
     fw2.close()
 
-    //    val fw = new PrintWriter("/home/davi/wcs/tese/dataset-tables-reduxes.tex", "ISO-8859-1")
-    //    val maisDesbalanceadas = todas.filter(x => x._2(4).toDouble > 4 * x._2(5).toDouble).toList.sortBy(x => x._2(4).toDouble / x._2(5).toDouble).reverse
-    //    fw.write(tabela("tab:imb", "Bases de dados mais desbalanceadas.", maisDesbalanceadas))
-    //
-    //    val maisAtributos = todas.filter(x => x._2(2).toDouble > 50).toList.sortBy(x => x._2(2).toDouble).reverse
-    //    fw.write(tabela("tab:x", "Bases de dados com mais atributos.", maisAtributos))
-    //
-    //    val maisClasses = todas.filter(x => x._2(1).toDouble > 5).toList.sortBy(x => x._2(1).toDouble).reverse
-    //    fw.write(tabela("tab:y", "Bases de dados com mais classes.", maisClasses))
-    //
-    //    val maisExemplos = todas.filter(x => x._2(0).toDouble > 1000).toList.sortBy(x => x._2(0).toDouble).reverse
-    //    fw.write(tabela("tab:n", "Bases de dados com mais exemplos.", maisExemplos))
-    //
-    //    val menosExemplos = todas.filter(x => x._2(0).toDouble < 400).toList.sortBy(x => x._2(0).toDouble)
-    //    fw.write(tabela("tab:nm", "Bases de dados com menos exemplos.", menosExemplos))
-    //    fw.close()
+    val fw = new PrintWriter("/home/davi/wcs/tese/dataset-tables-reduxes.tex") //, "ISO-8859-1")
+    val maisDesbalanceadas = todas.filter(x => x._2(4).toDouble > 5 * x._2(5).toDouble).toList.sortBy(x => x._2(4).toDouble / x._2(5).toDouble).reverse
+    fw.write(tabela("tab:imb", "Bases de dados mais desbalanceadas.", maisDesbalanceadas))
+
+    val maisAtributos = todas.filter(x => x._2(2).toDouble > 50).toList.sortBy(x => x._2(2).toDouble).reverse
+    fw.write(tabela("tab:x", "Bases de dados com mais atributos.", maisAtributos))
+
+    val maisClasses = todas.filter(x => x._2(1).toDouble > 6).toList.sortBy(x => x._2(1).toDouble).reverse
+    fw.write(tabela("tab:y", "Bases de dados com mais classes.", maisClasses))
+
+    val maisExemplos = todas.filter(x => x._2(0).toDouble > 3000).toList.sortBy(x => x._2(0).toDouble).reverse
+    fw.write(tabela("tab:n", "Bases de dados com mais exemplos.", maisExemplos))
+
+    val menosExemplos = todas.filter(x => x._2(0).toDouble < 300).toList.sortBy(x => x._2(0).toDouble)
+    fw.write(tabela("tab:nm", "Bases de dados com menos exemplos.", menosExemplos))
+    fw.close()
   }
 }
