3f1854b9284903ab181aff7d0f6fdc961c9610e7
davips <davips@users.noreply.github.com>
2015-10-22 16:02:37 -0200

    ordem por 2 max

--- src/main/scala/clean/tex/similaridadesDss.scala
+++ src/main/scala/clean/tex/similaridadesDss.scala
@@ -96,7 +96,7 @@ object similaridadesDss extends AppWithUsage with LearnerTrait with StratsTrait 
     }
 
     {
-      val sorted = m.map(_._2).sortBy(x => x.sum).transpose.sortBy(x => x.zipWithIndex.find(_._1 == 1).get._2).transpose
+      val sorted = m.map(_._2).sortBy(x => x.sorted.reverse(1)).transpose.sortBy(x => x.zipWithIndex.find(_._1 == 1).get._2).transpose
       val poeLea = m.map(x => x._2.sorted -> x._1).toMap
       val msorted = sorted map { x => poeLea(x.sorted) -> x }
       println("\n\n\n\n\n\n\n\n")
