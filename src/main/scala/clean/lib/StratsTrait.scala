86c7644daeb85dae39d58e6287e7bc8b4ecdb02c
davips <davips@users.noreply.github.com>
2015-10-26 23:59:46 -0200

    stratsTexForGraficoComplexo sem maha

--- src/main/scala/clean/lib/StratsTrait.scala
+++ src/main/scala/clean/lib/StratsTrait.scala
@@ -112,23 +112,23 @@ trait StratsTrait {
     Seq(
       if (dist == "eucl" || dist == "all") Some((learner: Learner) => AgDensityWeightedTrainingUtility(fakePool, "eucl")) else None
       , if (dist == "manh" || dist == "all") Some((learner: Learner) => AgDensityWeightedTrainingUtility(fakePool, "manh")) else None
-      , if (dist == "maha" || dist == "all") Some((learner: Learner) => AgDensityWeightedTrainingUtility(fakePool, "maha")) else None
+      //      , if (dist == "maha" || dist == "all") Some((learner: Learner) => AgDensityWeightedTrainingUtility(fakePool, "maha")) else None
       , Some((learner: Learner) => ExpErrorReductionMarginFixo(learner, fakePool, "balacc"))
       , Some((learner: Learner) => ExpErrorReductionMarginFixo(learner, fakePool, "entropy"))
       , Some((learner: Learner) => ClusterBased(fakePool)) //1
       , if (dist == "eucl" || dist == "all") Some((learner: Learner) => HTUFixo(fakePool, learner, fakePool, "eucl")) else None
       , if (dist == "manh" || dist == "all") Some((learner: Learner) => HTUFixo(fakePool, learner, fakePool, "manh")) else None
-      , if (dist == "maha" || dist == "all") Some((learner: Learner) => HTUFixo(fakePool, learner, fakePool, "maha")) else None
+      //      , if (dist == "maha" || dist == "all") Some((learner: Learner) => HTUFixo(fakePool, learner, fakePool, "maha")) else None
       , Some((learner: Learner) => MarginFixo(learner, fakePool))
       , Some((learner: Learner) => RandomSampling(fakePool)) //0
       , Some((learner: Learner) => SGmultiFixo(learner, fakePool, "consensus"))
       , if (dist == "eucl" || dist == "all") Some((learner: Learner) => DensityWeightedTrainingUtilityFixo(fakePool, learner, fakePool, "eucl")) else None
       , if (dist == "manh" || dist == "all") Some((learner: Learner) => DensityWeightedTrainingUtilityFixo(fakePool, learner, fakePool, "manh")) else None
-      , if (dist == "maha" || dist == "all") Some((learner: Learner) => DensityWeightedTrainingUtilityFixo(fakePool, learner, fakePool, "maha")) else None
+      //      , if (dist == "maha" || dist == "all") Some((learner: Learner) => DensityWeightedTrainingUtilityFixo(fakePool, learner, fakePool, "maha")) else None
 
       , if (dist == "eucl" || dist == "all") Some((learner: Learner) => DensityWeightedFixo(fakePool, learner, fakePool, 1, "eucl")) else None
       , if (dist == "manh" || dist == "all") Some((learner: Learner) => DensityWeightedFixo(fakePool, learner, fakePool, 1, "manh")) else None
-      , if (dist == "maha" || dist == "all") Some((learner: Learner) => DensityWeightedFixo(fakePool, learner, fakePool, 1, "maha")) else None
+      //      , if (dist == "maha" || dist == "all") Some((learner: Learner) => DensityWeightedFixo(fakePool, learner, fakePool, 1, "maha")) else None
 
     ).flatten
   }
