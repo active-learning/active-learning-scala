25dc47e5e7b21a1f96383b4100e2c4ab8625263a
davips <davips@users.noreply.github.com>
2015-11-06 17:52:50 -0200

    r -> run f ->

--- src/main/scala/ml/classifiers/MetaLearner.scala
+++ src/main/scala/ml/classifiers/MetaLearner.scala
@@ -58,8 +58,8 @@ case class MetaLearner(pool: Seq[Pattern], fpool: Seq[Pattern], todos: Map[Int, 
    +-------+-------------+------+-----+---------+-------+
      */
     val sqls = Seq(
-      s"select pre from e where ds='$ds' and fs=$fs and i='ti' and f='th' and st='${st.limp}' and leas='$leas' and mc='$mc' and r=$r and f=$f;",
-      s"select pre from e where ds='$ds' and fs=$fs and i='th' and f='tf' and st='${st.limp}' and leas='$leas' and mc='$mc' and r=$r and f=$f;"
+      s"select pre from e where ds='$ds' and fs=$fs and i='ti' and f='th' and st='${st.limp}' and leas='$leas' and mc='$mc' and run=$r and fold=$f;",
+      s"select pre from e where ds='$ds' and fs=$fs and i='th' and f='tf' and st='${st.limp}' and leas='$leas' and mc='$mc' and run=$r and fold=$f;"
     )
     val reses = sqls map { sql =>
       print(s"metasql: $sql\t\t")
