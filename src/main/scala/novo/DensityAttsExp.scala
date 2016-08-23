d1debd0fb1fffa35e57667137a97b691fb8ad67e
davips <dpsabc@gmail.com>
2016-08-22 23:11:35 -0300

    dens as att

--- src/main/scala/novo/DensityAttsExp.scala
+++ src/main/scala/novo/DensityAttsExp.scala
@@ -1,45 +1,50 @@
 package novo
 
 import clean.lib.{CM, Ds, Global}
-import ml.classifiers.RF
-import util.Datasets
+import ml.classifiers.{Maj, RF}
+import util.{Datasets, Stat}
 
 import scala.io.Source
 import scala.util.Random
 
 object DensityAttsExp extends App with CM with DistT {
-  val (rs, k) = (5, 5)
-  Global.debug = 100
+  val (rs, k) = (10, 10)
+  Global.debug = -100
   val q = 20
   val parallel = args.head.contains('p')
   val datasets = Source.fromFile(args(1)).getLines().toList
-  val trees = 20
-  val accs = processa(add = false, parallel = parallel)("banana")
-  //  val accs = datasets map processa(add = false, parallel = parallel)
+  val trees = 100
+
+    Seq("iris", "wine", "banana") foreach processa(add = false, parallel = parallel)
+//  datasets foreach processa(add = false, parallel = parallel)
+
   //  val accs2 = datasets map processa(add = true, parallel = parallel)
   //  val accs2 = processa(add = true, parallel = parallel)("iris")
 
   def processa(add: Boolean, parallel: Boolean)(dataset: String) = {
-    println(dataset)
+    print(dataset + " ")
     val ds = Ds(dataset, readOnly = true)
     ds.open()
-    val patts = if (add) addAtt(dataset, ds.patterns, ds.patterns) else ds.patterns
+    val patts = /*if (add) addAtt(dataset, ds.patterns, ds.patterns) else*/ ds.patterns
     ds.close()
     val runs = (1 to rs).par map { run =>
       val shuffled = new Random(run).shuffle(patts)
       Datasets.kfoldCV(shuffled, k, parallel) { (pool, ts, fold, minSize) =>
         val step = run - 1 + "." + fold
-        println(step + " Calculando densidades... ")
         val seed = 1000 * run + fold
-        val (newPool, newTs) = addAtt(dataset + step, pool, pool) -> addAtt(dataset + step, ts, ts ++ pool)
-        println(step + " ok!")
+        val ((newPool, newPool2), (newTs, newTs2)) = addAtt(dataset + step + "tr", pool, pool) -> addAtt(dataset + step + "ts", ts, pool)
+        //        println(step + " ok!")
         val l = RF(seed, trees)
-        val (m, m2) = l.build(pool) -> l.build(newPool)
-        accBal(m.confusion(ts)) -> accBal(m2.confusion(newTs))
+        val (m, m1, m2, mz) = (l.build(pool), l.build(newPool), l.build(newPool2), Maj().build(pool))
+        //normal junto sozinho zeroR
+        (accBal(m.confusion(ts)) -> accBal(m1.confusion(newTs)), accBal(m2.confusion(newTs2)) -> accBal(mz.confusion(ts)))
       }
     }
-    val (l1, l2) = runs.flatten.unzip
-    println(l1.sum / (rs * k) -> l2.sum / (rs * k))
+    val (la, lb) = runs.flatten.unzip
+    val (nor, jun) = la.unzip
+    val (soz, zer) = lb.unzip
+    Seq(nor, jun, soz, zer) foreach (x => print(Stat.media_desvioPadrao(x.toVector) + " "))
+    println
   }
 
   val context: String = "eixos exp"
--- src/main/scala/novo/Dist.scala
+++ src/main/scala/novo/Dist.scala
@@ -87,30 +87,44 @@ case class Dist(pool: Seq[Pattern]) {
     }
 }
 
-trait DistT{
+trait DistT {
   def addAtt(dataset: String, patts: Seq[Pattern], allpatts: Seq[Pattern]) = {
     val seqden = Seq(1, 4, 16, 32, 64, 128)
     val di = Dist(allpatts)
     val header = patts.head.dataset.toString.split("\n").takeWhile(!_.contains("@data"))
     val atts = seqden map (i => s"@attribute d$i numeric")
-    val newHeader = header.dropRight(2) ++ atts ++ header.takeRight(2)
+    val (newHeader, newHeader2) = (header.dropRight(2) ++ atts ++ header.takeRight(2), header.take(1) ++ atts ++ header.takeRight(2))
 
     def denses(p: Pattern) = seqden map den(di, allpatts, p)
-    val newData = patts map { p => (p.toString.split(",").dropRight(1) ++ denses(p) ++ p.toString.split(",").takeRight(1)).mkString(",") }
+    val (newData, newData2) = (patts.par map { p =>
+      val dss = denses(p)
+      (p.toString.split(",").dropRight(1) ++ dss ++ p.toString.split(",").takeRight(1)).mkString(",") ->
+        (dss ++ p.toString.split(",").takeRight(1)).mkString(",")
+    }).unzip
     val arq = s"/run/shm/$dataset.arff"
 
     val fw = new FileWriter(arq)
     fw.write((newHeader ++ Seq("@data") ++ newData).mkString("\n"))
     fw.close()
-    Datasets.arff(arq, dedup = false, rmuseless = false) match {
+    val res = Datasets.arff(arq, dedup = false, rmuseless = false) match {
       case Right(x) => x
       case Left(e) => sys.error(e)
     }
+
+    val fw2 = new FileWriter(arq + "2.arff")
+    fw2.write((newHeader2 ++ Seq("@data") ++ newData2).mkString("\n"))
+    fw2.close()
+    val res2 = Datasets.arff(arq + "2.arff", dedup = false, rmuseless = false) match {
+      case Right(x) => x
+      case Left(e) => sys.error(e)
+    }
+
+    res -> res2
   }
 
   def den(di: Dist, pool: Seq[Pattern], x: Pattern)(numNeigs: Int) = {
-    val simis = pool map { s => s -> 1d / (1 + di.d(x, s)) }
-    val neigs = simis.sortBy(-_._2).take(numNeigs + 1).tail.map(_._2)
+    val simis = pool.par map { s => s -> 1d / (1 + di.d(x, s)) }
+    val neigs = simis.toList.sortBy(-_._2).take(numNeigs + 1).tail.map(_._2)
     neigs.sum / numNeigs
   }
 }
