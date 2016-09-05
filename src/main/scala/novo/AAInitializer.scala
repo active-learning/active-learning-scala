b0598f2f05026ffd3736a3485954cd3e9bf7796f
davips <dpsabc@gmail.com>
2016-09-05 15:09:02 -0300

    res.distinct -> res2.distinct ++

--- src/main/scala/novo/AAInitializer.scala
+++ src/main/scala/novo/AAInitializer.scala
@@ -1,7 +1,6 @@
 package novo
 
 import al.strategies.RandomSampling
-import clean.lib.Ds
 import ml.Pattern
 
 import scala.collection.mutable
@@ -10,7 +9,7 @@ trait AAInitializer {
   def initialSet(pool: Seq[Pattern]) = {
     val Y = pool.head.nclasses
     val set = mutable.Set[Double]()
-    val rnd = RandomSampling(pool)
+    val rnd = RandomSampling(pool.distinct)
     val qs = rnd.queries.takeWhile { p =>
       set += p.label
       set.size < Y
