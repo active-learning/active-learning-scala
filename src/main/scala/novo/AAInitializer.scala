b8f7187159de8bfcc736f436bbd6f384296566a3
davips <dpsabc@gmail.com>
2016-09-05 15:17:05 -0300

    verify before rnd

--- src/main/scala/novo/AAInitializer.scala
+++ src/main/scala/novo/AAInitializer.scala
@@ -9,11 +9,17 @@ trait AAInitializer {
   def initialSet(pool: Seq[Pattern]) = {
     val Y = pool.head.nclasses
     val set = mutable.Set[Double]()
-    val rnd = RandomSampling(pool.distinct)
-    val qs = rnd.queries.takeWhile { p =>
-      set += p.label
-      set.size < Y
+    val firstof_each_class = (0 until Y).flatMap { c =>
+      pool find (_.label == c)
+    }.toList
+    if (firstof_each_class.size < Y) Seq()
+    else {
+      val rnd = RandomSampling(pool)
+      val qs = rnd.queries.takeWhile { p =>
+        set += p.label
+        set.size < Y
+      }
+      rnd.queries.take(qs.toList.length + 1).toList
     }
-    rnd.queries.take(qs.toList.length + 1).toList
   }
 }
