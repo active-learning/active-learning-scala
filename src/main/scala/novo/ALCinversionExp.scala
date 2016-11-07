35847eb902c2cb059f2c92555a0d2d86b3852f0b
davips <dpsabc@gmail.com>
2016-11-07 16:09:38 -0200

    copiei um Exp moderno pra servir de base pro ALCinversion

--- src/main/scala/novo/ALCinversionExp.scala
+++ src/main/scala/novo/ALCinversionExp.scala
@@ -1,55 +1,88 @@
 package novo
 
-import al.strategies._
+import al.strategies.TU
 import clean.lib.{CM, Ds, Global}
 import ml.Pattern
-import ml.classifiers.{Learner, RF}
-import util.Datasets
+import ml.classifiers.{Learner, Maj, RF}
+import util.{Datasets, Stat}
 
 import scala.util.Random
 
-object ALCinversionExp extends App with CM with AAInitializer{
-  val (rs, k) = (2, 2)
-  Global.debug = 100
-  val q = 250
-  val parallel = args.head.contains('p')
-  val datasets = args.tail
-  val trees = 25
-  datasets foreach { case dataset =>
-    val ds = Ds(dataset, readOnly = true)
-    ds.open()
-    val runs = 1 to rs map { run =>
-      val shuffled = new Random(run).shuffle(ds.patterns)
-      processa(ds, run, shuffled, k, parallel)
+object ALCinversionExp extends Args with CM with DistT with AAInitializer {
+  val context: String = "datt exp"
+  lazy val exp = getClass.getSimpleName + ": " + args.filter(!_.startsWith("clear=")).sorted.mkString(" ")
+  run()
+
+  def processa(f: (String, Seq[Pattern], Seq[Pattern]) => (scala.Vector[Pattern], scala.Vector[Pattern]), parallel: Boolean)(dataset: String) = {
+    def exe(patts: Vector[Pattern], preAdded: Boolean) = {
+      val runs = (if (argb("parr")) (1 to argi("runs")).par else 1 to argi("runs")) map { run =>
+        val shuffled = new Random(run).shuffle(patts)
+        Datasets.kfoldCV(shuffled, argi("k"), parallel) { (pool, ts, fold, minSize) =>
+          val step = run - 1 + "." + fold
+          val seed = 1000 * run + fold
+          val l = RF(seed, argi("trees"), argi("trees") / 2)
+          if (pool.size<5) (0d -> 0d, 0d -> 0d)
+          else {
+            if (preAdded) (alc(l, pool, ts) -> -1d, -1d -> -1d)
+            else {
+              val ((newpool, newpoolOnlyDens), (newTs, newTsOnlyDens)) = f(dataset + step + "tr", pool, pool) -> f(dataset + step + "ts", ts, pool)
+              //normal junto sozinho zeroR
+              (alc(l, pool, ts) -> alc(l, newpool, newTs), alc(l, newpoolOnlyDens, newTsOnlyDens) -> alc(Maj(), pool, ts))
+            }
+          }
+        }
+      }
+      val (la, lb) = runs.flatten.unzip
+      val (nor, jun) = la.unzip
+      val (soz, zer) = lb.unzip
+      Seq(nor.toVector, jun.toVector, soz.toVector, zer.toVector) map Stat.media_desvioPadrao
     }
-    val allRuns = runs.flatten
-    val sums = allRuns.map(_.flatten).reduce((a, b) => a.zip(b).map { case (x, y) => x + y })
-    val divs = sums.map(_ / (rs * k).toDouble)
-    divs.grouped(q).toList.transpose foreach (x => println(x.mkString(" ")))
-    ds.close()
+
+    print(dataset + " ")
+    val alive = ALive(dataset, exp)
+    if (argb("clear")) alive.clear()
+    alive.getResults match {
+      case Some(str) => println(str)
+      case None =>
+        if (alive.isFree) {
+          alive.start()
+          val ds = Ds(dataset, readOnly = true)
+          ds.open()
+          val (patts, (newPatts, newPattsOnlyDens)) = ds.patterns -> f(dataset, ds.patterns, ds.patterns)
+          ds.close()
+          val nojusoze = exe(patts, preAdded = false)
+          val juPreAdded = exe(newPatts, preAdded = true).filter(_._1 > -1d)
+          val soPreAdded = exe(newPattsOnlyDens, preAdded = true).filter(_._1 > -1d)
+          val res = (nojusoze ++ juPreAdded ++ soPreAdded) map (x => (1000 * x._1).round / 1000d + "/" + (1000 * x._2).round / 1000d + " ")
+          println(res.mkString)
+          alive.putResults(res.mkString)
+          alive.stop()
+        } else println("busy")
+    }
   }
 
-  def strats(pool: Vector[Pattern], l: Learner) = List(
-    Rnd(pool), Mar(l, pool), SG(l, pool, "consensus"),
-    EER(l, pool, "entropy"), TU(pool, l, pool), HS(pool)
-  )
+  def run() = try {
+    Global.debug = argi("log")
+    println(exp)
+    val f = if (argb("1d")) addAtt1d _ else addAtt _
+    argl.getOrElse("file", argl("datasets")) foreach processa(f, argb("parf"))
+  } catch {
+    case e: Throwable =>
+      e.printStackTrace()
+      Thread.sleep(100)
+      println(e.getClass.getName + " " + e.getMessage)
+  }
 
-  def processa(ds: Ds, run: Int, shuffled: Vector[Pattern], k: Int, parallel: Boolean) = Datasets.kfoldCV(shuffled, k, parallel) { (pool0, ts, fold, minSize) =>
-    val seed = 1000 * run + fold
-    val pool = new Random(seed).shuffle(pool0.sortBy(_.id))
+  def alc(l: Learner, pool: Vector[Pattern], ts: Vector[Pattern]) = if (pool.size<10) 0d else {
+    val s = TU(pool, l, pool)
     val labeled = initialSet(pool)
     val unlabeled = pool.diff(labeled)
-    val l = RF(seed, trees)
-    val res = strats(pool, l).map { s =>
-      val queries = s.queries_noLabels(unlabeled, labeled).take(q - labeled.size).toList
-      1 to q map { i => //seq
-        val m = l.build(queries.take(i))
-        accBal(m.confusion(ts))
-      }
+    val queries = s.queries_noLabels(unlabeled, labeled).take(argi("q") - labeled.size).toVector
+    val vs = 1 to argi("q") map { i =>
+      val m = l.build(queries.take(i))
+      accBal(m.confusion(ts))
     }
-    println(res)
-    res
-  }.toVector
+    vs.sum / vs.size
+  }
 
-  val context: String = "eixos exp"
 }
