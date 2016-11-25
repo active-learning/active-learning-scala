f4d775a2b8e8a91d470e23e14fbd0c3ddb83d059
davips <dpsabc@gmail.com>
2016-11-25 16:49:29 -0200

    arg neigs for 1 item

--- src/main/scala/novo/Args.scala
+++ src/main/scala/novo/Args.scala
@@ -9,7 +9,7 @@ import scala.util.Random
 
 trait Args extends App {
   private lazy val argsb = args filter (x => x.endsWith("=y") || x.endsWith("=n"))
-  private lazy val argsn = args filter (x => x.split('=').last.filter(x => x != '.' && x != '-' ).forall(x => x.isDigit))
+  private lazy val argsn = args filter (x => x.split('=').last.filter(x => x != '.' && x != '-').forall(x => x.isDigit))
   private lazy val argsl = args filter (x => x.contains(",") || x.startsWith("file=") || x.startsWith("datasets="))
   private lazy val argst = args diff argsb diff argsn diff argsl
 
@@ -17,6 +17,7 @@ trait Args extends App {
   lazy val argi = argsn map parse map (x => x._1 -> x._2.toInt) toMap
   lazy val argl = argsl map parse map {
     case ("file", file) => "file" -> Source.fromFile(file).getLines().toList
+    case ("neigs", n) if !n.contains(",") => "neigs" -> List(n)
     case (k, v) => k -> v.split(',').toList
   } toMap
   lazy val argt = argst map parse toMap
